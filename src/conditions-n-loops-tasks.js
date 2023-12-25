85a030c8e80df9ab63c848105c427cda93ad0963
vladimirovicp <vladimirovicp70@gmail.com>
2023-12-25 16:37:01 +0400

    add: 3

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -70,8 +70,15 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) {
+    return true;
+  }
+  if (queen.y === king.y) {
+    return true;
+  }
+
+  return Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y);
 }
 
 /**
